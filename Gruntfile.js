292aa67ab26ddfae362256b44596e65b62b4cf66
stefanocudini <stefano.cudini@gmail.com>
2015-07-26 00:59:49 +0200

    gruntfile uglify

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,12 +2,41 @@
 
 module.exports = function(grunt) {
 
+grunt.loadNpmTasks('grunt-contrib-clean');
+grunt.loadNpmTasks('grunt-contrib-jshint');
+grunt.loadNpmTasks('grunt-contrib-watch');
+grunt.loadNpmTasks('grunt-contrib-concat');
+grunt.loadNpmTasks('grunt-contrib-uglify');
+grunt.loadNpmTasks('grunt-markdown');
+
 grunt.initConfig({
 	pkg: grunt.file.readJSON('package.json'),
+	meta: {
+		banner:
+		'/* \n'+
+		' * Leaflet Location Picker v<%= pkg.version %> - <%= grunt.template.today("yyyy-mm-dd") %> \n'+
+		' * \n'+
+		' * Copyright <%= grunt.template.today("yyyy") %> <%= pkg.author.name %> \n'+
+		' * <%= pkg.author.email %> \n'+
+		' * <%= pkg.author.url %> \n'+
+		' * \n'+
+		' * Licensed under the <%= pkg.license %> license. \n'+
+		' * \n'+
+		' * Demo: \n'+
+		' * <%= pkg.homepage %> \n'+
+		' * \n'+
+		' * Source: \n'+
+		' * <%= pkg.repository.url %> \n'+
+		' * \n'+
+		' */\n'
+	},	
 	clean: {
-		dist: {
+		homepage: {
 			src: ['index.html']
-		}
+		},
+		dist: {
+			src: ['dist/*']
+		}		
 	},
 	jshint: {
 		options: {
@@ -19,7 +48,29 @@ grunt.initConfig({
 			"-W033": true,
 			"-W044": true	//ignore regexp
 		},
-		files: ["<%= pkg.name %>.js"]
+		files: ["src/<%= pkg.name %>.js"]
+	},
+	concat: {
+		//TODO cut out SearchMarker
+		options: {
+			banner: '<%= meta.banner %>'
+		},
+		dist: {
+			files: {
+				'dist/<%= pkg.name %>.src.js': ['src/<%= pkg.name %>.js'],
+				'dist/<%= pkg.name %>.src.css': ['src/<%= pkg.name %>.css']
+			}
+		}
+	},
+	uglify: {
+		options: {
+			banner: '<%= meta.banner %>'
+		},
+		dist: {
+			files: {
+				'dist/<%= pkg.name %>.min.js': ['dist/<%= pkg.name %>.src.js']
+			}
+		}
 	},
 	markdown: {
 		readme: {
@@ -27,16 +78,22 @@ grunt.initConfig({
 				'index.html': ['README.md']
 			}
 		}
-	}
+	},
+	watch: {
+		dist: {
+			options: { livereload: true },
+			files: ['src/*'],
+			tasks: ['clean','concat','jshint']
+		}		
+	}	
 });
 
 grunt.registerTask('default', [
 	'clean',
+	'concat',
 	'jshint',
+	'uglify',
 	'markdown'
 ]);
 
-grunt.loadNpmTasks('grunt-contrib-clean');
-grunt.loadNpmTasks('grunt-contrib-jshint');
-grunt.loadNpmTasks('grunt-markdown');
 };
